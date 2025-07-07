192d5cb99fbd4c4e05e02ff40e943b49f9142ca7
HAnh3112 <haianh311205@gmail.com>
2025-07-07 09:10:47 +0700

    Created basic repository interface for each model and table

---
+++ src/main/java/com/example/JavaWebFinal/repository/TransactionRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.JavaWebFinal.repository;
+
+/**
+ *
+ * @author ADMIN
+ */
+import com.example.JavaWebFinal.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    // You can define custom query methods here if needed
+}
+
